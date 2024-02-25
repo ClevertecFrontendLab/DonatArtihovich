36c785fafd4606c438caa249d8eb45bd8a3b9dcb
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2024-02-25 18:53:20 +0300

    feat: add change password page

--- src/pages/change-password/ui/change-password.tsx
+++ src/pages/change-password/ui/change-password.tsx
@@ -1,10 +1,37 @@
 import { ModalPage } from "@components/modal"
+import cls from './change-password.module.scss'
+import { Button, Form, Typography } from "antd"
+import Password from "antd/lib/input/Password"
+import { useWindowSize } from "@uidotdev/usehooks"
 
 export const ChangePasswordPage = () => {
+    const { width } = useWindowSize()
 
     return (
-        <ModalPage>
-            <></>
+        <ModalPage className={cls.modal}>
+            <div className={cls.wrapper}>
+                <Typography.Title className={cls.headerTitle}>Восстановление{width && width <= 600 && <br />} аккаунта</Typography.Title>
+                <Form className={cls.passwordForm}>
+                    <Form.Item
+                        name='password'
+                        help='Пароль не менее 8 символов, с заглавной буквой и цифрой'
+                        className={cls.passwordInputWrapper}
+                    >
+                        <Password
+                            className={cls.passwordInput}
+                            placeholder="Новый пароль"
+                        />
+                    </Form.Item>
+                    <Form.Item name='confirmPassword' className={cls.passwordInputWrapper}>
+                        <Password
+                            className={cls.passwordInput}
+                            placeholder="Повторите пароль"
+                        />
+                    </Form.Item>
+
+                    <Button className={cls.submitButton}>Сохранить</Button>
+                </Form>
+            </div>
         </ModalPage>
     )
 }
