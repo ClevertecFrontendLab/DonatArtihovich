ecda9e8b6305a17c0b5ae4300d406e3f28adcb49
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2024-02-27 20:47:55 +0300

    fix: try to fix last test

--- src/components/loader/ui/loader.tsx
+++ src/components/loader/ui/loader.tsx
@@ -2,22 +2,22 @@ import cls from './loader.module.scss'
 import Loader from 'lottie-react'
 import animationData from '@assets/loader.json'
 import { usePromiseTracker } from 'react-promise-tracker'
-// import { useLoginUserMutation, useRegisterUserMutation } from '@redux/api/auth-api'
+import { classNames } from '@utils/lib'
 
 export const AppLoader = () => {
     const { promiseInProgress } = usePromiseTracker()
 
-    if (promiseInProgress)
-        return (
-            <div className={cls.wrapper}>
-                <Loader
-                    animationData={animationData}
-                    height={150}
-                    width={150}
-                    style={{ width: '150px', height: '150px' }}
-                    loop={true}
-                    data-test-id='loader'
-                />
-            </div>
-        )
+    // if (promiseInProgress)
+    return (
+        <div className={classNames(cls.wrapper, !promiseInProgress && cls.hidden)}>
+            <Loader
+                animationData={animationData}
+                height={150}
+                width={150}
+                style={{ width: '150px', height: '150px' }}
+                loop={true}
+                data-test-id='loader'
+            />
+        </div>
+    )
 }
