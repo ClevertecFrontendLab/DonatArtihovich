13fe819dc6dca9bf856fb37d3d27155ebe288b0a
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2024-03-01 00:56:50 +0300

    fix: fix loader

--- src/components/loader/ui/loader.tsx
+++ src/components/loader/ui/loader.tsx
@@ -3,11 +3,19 @@ import Loader from 'lottie-react'
 import animationData from '@assets/loader.json'
 import { usePromiseTracker } from 'react-promise-tracker'
 import { classNames } from '@utils/lib'
+import { useEffect } from 'react'
 
 export const AppLoader = () => {
     const { promiseInProgress } = usePromiseTracker()
 
-    // if (promiseInProgress)
+    useEffect(() => {
+        if (promiseInProgress) {
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = 'auto'
+        }
+    }, [promiseInProgress])
+
     return (
         <div className={classNames(cls.wrapper, !promiseInProgress && cls.hidden)}>
             <Loader
