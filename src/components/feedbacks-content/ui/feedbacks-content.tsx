83ee03f0e99235267f06f2bfe0d19b7ca6091f52
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2024-03-02 20:39:04 +0300

    feat: add no feedbacks modal

--- src/components/feedbacks-content/ui/feedbacks-content.tsx
+++ src/components/feedbacks-content/ui/feedbacks-content.tsx
@@ -2,7 +2,7 @@ import { FeedbackCard } from "@components/feedback-card"
 import { FeedbackType } from "@redux/feedbacks/types"
 import cls from './feedbacks-content.module.scss'
 import { Content } from "antd/lib/layout/layout"
-import { Button } from "antd"
+import { Button, Typography } from "antd"
 import { useAppDispatch, useAppSelector } from "@hooks/typed-react-redux-hooks"
 import { useNavigate } from "react-router-dom"
 import { useEffect } from "react"
@@ -13,6 +13,8 @@ import { useRequiredContext } from "@hooks/typed-use-context-hook"
 import { ModalContext } from "@processes/modal"
 import { ModalErrors } from "@utils/const/modal-errors"
 import { AppLoader } from "@components/loader"
+import { useWindowSize } from "@uidotdev/usehooks"
+import { classNames } from "@utils/lib"
 
 type FeedbacksContent = {
     isSiderCollapsed: boolean;
@@ -20,11 +22,12 @@ type FeedbacksContent = {
 
 export const FeedbacksContent = ({ isSiderCollapsed }: FeedbacksContent) => {
     const { token } = useAppSelector(userSelector)
+    const { width } = useWindowSize()
     const navigate = useNavigate()
     const dispatch = useAppDispatch()
     const { setMode } = useRequiredContext(ModalContext)
-    const { data: feedbacks, isFetching: isFeedbacksFetching, isError: isFeedbacksError } = useGetFeedbacksQuery({})
-
+    const { /*data: feedbacks,*/ isFetching: isFeedbacksFetching, isError: isFeedbacksError } = useGetFeedbacksQuery({})
+    const feedbacks: FeedbackType[] = []
     useEffect(() => {
         if (!token) {
             const storageToken = localStorage.getItem('user')
@@ -45,27 +48,37 @@ export const FeedbacksContent = ({ isSiderCollapsed }: FeedbacksContent) => {
     return (
         <Content>
             <AppLoader isLoader={isFeedbacksFetching} />
-            <div className={cls.feedbacksContent}>
-                <div className={cls.feedbacksList}>
-                    {(feedbacks && feedbacks.length ? feedbacks.slice(-4) : [{
-                        id: 'hdfjs',
-                        fullName: 'Donat',
-                        imageSrc: null,
-                        message: 'message '.repeat(210),
-                        rating: 4,
-                        createdAt: '2023-04-12'
-                    }]).map((feedback: FeedbackType) => <FeedbackCard
-                        key={feedback.id}
-                        feedback={feedback}
-                        isSiderCollapsed={isSiderCollapsed}
-                    />)}
-                </div>
-                <div className={cls.buttonsWrapper}>
-                    <Button className={cls.writeButton}>Написать отзыв</Button>
-                    <Button className={cls.allFeedbacksButton}>Развернуть все отзывы</Button>
-                </div>
-            </div>
+            {feedbacks && <div className={classNames(
+                cls.feedbacksContent,
+                !feedbacks.length && cls.noFeedbacks,
+                isSiderCollapsed && cls.stretched
+            )}>
+                {feedbacks.length ?
+                    <>
+                        <div className={cls.feedbacksList}>
+                            {feedbacks.slice(-4).map((feedback: FeedbackType) => <FeedbackCard
+                                key={feedback.id}
+                                feedback={feedback}
+                                isSiderCollapsed={isSiderCollapsed}
+                            />)}
+                        </div>
+                        <div className={cls.buttonsWrapper}>
+                            <Button className={cls.writeButton}>Написать отзыв</Button>
+                            <Button className={cls.allFeedbacksButton}>Развернуть все отзывы</Button>
+                        </div>
+                    </>
+                    : <>
 
+                        <div className={cls.NoFeedbacksContent}>
+                            <div className={cls.NoFeedbacksModal}>
+                                <Typography.Title className={cls.NoFeedbacksTitle}>Оставьте свой отзыв первым</Typography.Title>
+                                <Typography.Text className={cls.NoFeedbacksText}>Вы можете быть первым, кто оставит отзыв об этом фитнесс приложении.<br />Поделитесь своим мнением и {width && width < 600 && <br />}опытом с другими пользователями,<br /> и помогите им сделать правильный выбор.</Typography.Text>
+                            </div>
+                            <Button className={cls.writeButton}>Написать отзыв</Button>
+                        </div>
+                    </>}
+            </div>
+            }
         </Content>
     )
 }
